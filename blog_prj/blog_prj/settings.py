ce99ce6e4a811acf7985b518fa2d3955234bf23a
Quinn Avila <qavila@drw.com>
2016-04-05 21:19:31 -0500

    tried adding disqus comments and added pictures

--- blog_prj/blog_prj/settings.py
+++ blog_prj/blog_prj/settings.py
@@ -15,6 +15,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -37,8 +39,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'django.contrib.sites',
+    'blog',
+    'disqus',
 )
+DISQUS_API_KEY = "k9DTzB0kgXascrW6MGSrCl5VhI9ZNoSzIHjaNeEuhfuMbCUDVECpeAarQkxK3O8K"
+DISQUS_WEBSITE_SHORTNAME = "mybootcampblog"
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
